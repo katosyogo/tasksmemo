a8bf730c81765f437541061bd222177eecb0387b
katosyogo <seiten.hkss321@gmail.com>
2020-02-18 15:09:59 +0900

    test2

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,25 +1,7 @@
 class MemosController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new, :create, :destroy, :edit, :update]
-  # # before_action :require_login, except: [:index, :show]
-  # def authenticate_user!
-  #   #ログアウト状態だったら
-  #   if @current_user == true
-  #     # flash[:notice] = "ログインが必要です"
-  #     redirect_to("/memos/show")
-  #   end
-  # end
 
   def index
-    @memos = Memo.where(user_id: current_user.id).order("created_at DESC")if user_signed_in?   #要検証
-    # if user_signed_in?
-    #   redirect_to "/"
-    # else
-    #   redirect_to "user_session_path"
-    # end
-  end
-
-  def show
-    # @memos = Memo.where(user_id: current_user.id).order("created_at DESC")   #要検証
+    @memos = Memo.where(user_id: current_user.id).order("updated_at DESC")if user_signed_in?   #要検証
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'memos#index'
-  resources :memos, only: [:index, :show, :new, :create, :destroy, :edit, :update]
+  resources :memos, only: [:index, :new, :create, :destroy, :edit, :update]
 end
